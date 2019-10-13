5f3c91278462720da91198e95ec0e78878ebd751
Kohei Yamaguchi <fix7211@gmail.com>
2019-10-14 01:56:52 +0900

    Add tools.

---
+++ tools/i386-elf-gcc/include/string.h
@@ -0,0 +1,10 @@
+#ifndef _STRING_H
+#define _STRING_H
+
+#include <stddef.h>
+
+char *strcpy(char *s1, const char *s2);
+char *strcat(char *s1, const char *s2);
+size_t strlen(const char *s);
+
+#endif
